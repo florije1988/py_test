03b72bdf27f02f148b89c8af9dca179ffb32b3fd
florije1988 <florije1988@gmail.com>
2015-07-09 17:13:21 +0800

    Initial commit

---
+++ app/calculator.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+__author__ = 'florije'
+
+
+class Calculator(object):
+    def add(self, x, y):
+        number_types = (int, long, float, complex)
+
+        if isinstance(x, number_types) and isinstance(y, number_types):
+            return x + y
+        else:
+            raise ValueError
